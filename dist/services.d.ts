8db881c1a9439528c91ce33dcf336de5d4ae4fa9
Wayne Van Son <waynevspersonal@gmail.com>
2019-12-25 21:18:23 +1100

    rebuild

--- dist/services.d.ts
+++ dist/services.d.ts
@@ -1,11 +1,9 @@
 import React, { PropsWithChildren } from "react";
-export interface ServiceProps<T> extends PropsWithChildren<{}> {
-    /** Use if you'd like to use some dummy data for your service */
-    test?: T;
-}
-export interface ServiceProviderProps<T> extends ServiceProps<T> {
+export interface ServiceProviderProps<T> {
     /** If provided, overrides the `useValue` hook originally used to generate this service. */
-    useTest: (props: ServiceProps<T>) => T;
+    useTest?: (props: PropsWithChildren<{
+        value: T;
+    }>) => T;
 }
 /**
  * Creates a service. Saves reqriting the same hooks over and over.
@@ -13,5 +11,5 @@ export interface ServiceProviderProps<T> extends ServiceProps<T> {
  * @param defaultValue the default value. You can make this whatever you like, but it defaults to null and throws.
  * @returns `[use[Service], [Service]provider, [service]Context]`, where `[service]` is the name of your service.
  */
-export declare function createService<T>(useValue: (props: ServiceProps<T>) => T, defaultValue?: T | null): readonly [() => NonNullable<T>, React.FC<ServiceProviderProps<T>>, React.Context<T | null>];
+export declare function createService<T>(useValue: (props: PropsWithChildren<{}>) => T, defaultValue?: T | null): readonly [() => NonNullable<T>, React.FC<ServiceProviderProps<T>>, React.Context<T | null>];
 //# sourceMappingURL=services.d.ts.map
--- dist/services.js
+++ dist/services.js
@@ -1,47 +1,28 @@
-"use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = __importStar(require("react"));
+import React, { createContext, useContext } from "react";
 /**
  * Creates a service. Saves reqriting the same hooks over and over.
  * @param useValue The value for your global state
  * @param defaultValue the default value. You can make this whatever you like, but it defaults to null and throws.
  * @returns `[use[Service], [Service]provider, [service]Context]`, where `[service]` is the name of your service.
  */
-function createService(useValue, defaultValue) {
+export function createService(useValue, defaultValue) {
     if (defaultValue === void 0) { defaultValue = null; }
-    var serviceContext = react_1.createContext(defaultValue);
+    var serviceContext = createContext(defaultValue);
     var ServiceProvider = function (_a) {
-        var useTest = _a.useTest, props = __rest(_a, ["useTest"]);
-        var value = useValue(props);
-        var tests = useTest(props);
-        return (react_1.default.createElement(serviceContext.Provider, { value: props.test || tests || value }, props.children));
+        var _b = _a.useTest, useTest = _b === void 0 ? function () {
+            return undefined;
+        } : _b, children = _a.children;
+        var value = useValue({ children: children });
+        var tests = useTest({ children: children, value: value });
+        return (React.createElement(serviceContext.Provider, { value: tests || value }, children));
     };
     //@todo - put a prop/option in here?
     var useService = function () {
-        var service = react_1.useContext(serviceContext);
+        var service = useContext(serviceContext);
         if (!service)
             throw new Error("'Your \"useContext\" in the service generator returned the invalid value \"" + service + "\"");
         return service;
     };
     return [useService, ServiceProvider, serviceContext];
 }
-exports.createService = createService;
 //# sourceMappingURL=services.js.map
